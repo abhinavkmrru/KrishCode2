0fc100835e7c5c9e4713d7ea1ec36ee35ff0035a
Abhinav <abhinav.kmr.ru@gmail.com>
2021-12-19 20:25:00 +0530

    Dialog box design

---
+++ DAY_e_2/script.js
@@ -0,0 +1,11 @@
+function closedialogbox(){
+    var dialog_container = document.getElementById("dialog_container");
+    dialog_container.style.display = "none";
+
+}
+
+function showDialog(){
+    var dialog_container = document.getElementById("dialog_container");
+    dialog_container.style.display = "block";
+}
+
